40d7b351f4ca8b3c15374f4fcd0212010e816299
Bernardo Pandolfi Costa <bernardopandolficosta@gmail.com>
2024-04-11 17:26:03 -0300

    routes and more server structure

---
+++ run.sh
@@ -0,0 +1,22 @@
+# Define image name (customize if needed)
+# IMAGE_NAME="server-apenados"
+
+# Build the Docker image
+# echo "Building Docker image..."
+cd server
+# docker compose build
+
+# # Check for build errors
+# if [ $? -eq 0 ]; then
+#   echo "Build successful!"
+# else
+#   echo "Build failed. Please check the logs for errors."
+#   exit 1
+# fi
+
+# Run the container in detached mode
+echo "Running the container..."
+docker compose up
+
+
+echo "Flask application is running! Access it at http://localhost:5000/"
--- server/app/app.py
+++ server/app/app.py
@@ -1,10 +1,41 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+import logging
 
 app = Flask(__name__)
 
+logging.basicConfig(level=logging.DEBUG)
+
+# Dummy user data (replace this with a proper user database)
+users = {
+    "a": "123",
+    "b": "123",
+}
+
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    logging.debug('Index route!')
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    logging.debug('Login route!')
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username in users and users[username] == password:
+            # Successful login, redirect to a protected route
+            logging.debug('Login successful!')
+            return redirect(url_for('protected'))
+        else:
+            # Login failed, redirect back to login page with a message
+            logging.debug('Login failed!')
+            return redirect(url_for('login'))
+    return render_template('login.html')
+
+@app.route('/protected')
+def protected():
+    logging.debug('Protected route!')
+    return "This is a protected route!"
 
 if __name__ == '__main__':
     app.run(debug=True)
