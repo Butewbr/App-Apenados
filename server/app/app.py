d6b7cf1a1a2a0d635812b692628a186d8c390b77
Bernardo Pandolfi Costa <bernardopandolficosta@gmail.com>
2024-04-17 15:24:31 -0300

    loads of progress

--- server/app/app.py
+++ server/app/app.py
@@ -1,42 +1,109 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required
 import logging
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'  # Change this to a random secret key
 
 logging.basicConfig(level=logging.DEBUG)
 
-# Dummy user data (replace this with a proper user database)
-users = {
-    "a": "123",
-    "b": "123",
-    "c": "123",
+# Dummy apenados data
+apenados = {
+    "Robert": "123",
+    "Alice": "456",
+    "John": "789",
+    "Emily": "246",
+    "Michael": "135",
+    "Sarah": "579",
+    "David": "802",
+    "Jennifer": "304",
+    "Daniel": "687",
+    "Jessica": "910",
+    "Matthew": "234",
+    "Amanda": "567",
+    "Christopher": "891",
+    "Michelle": "432",
+    "Andrew": "765"
 }
+
+# Dummy user data
+users = {
+    'user1': {'password': '123'},
+    'user2': {'password': '123'},
+    'user3': {'password': '123'},
+    'user4': {'password': '123'},
+    'user5': {'password': '123'}
+}
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+class User(UserMixin):
+    pass
 
 @app.route('/')
 def index():
     logging.debug('Index route!')
     return render_template('index.html')
 
+@login_manager.user_loader
+def load_user(username):
+    if username in users:
+        user = User()
+        user.id = username
+        return user
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    logging.debug('Login route!')
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        if username in users and users[username] == password:
-            # Successful login, redirect to a protected route
-            logging.debug('Login successful!')
-            return redirect(url_for('protected'))
-        else:
-            # Login failed, redirect back to login page with a message
-            logging.debug('Login failed!')
-            return redirect(url_for('login'))
+        if username in users and users[username]['password'] == password:
+            user = User()
+            user.id = username
+            login_user(user)
+            return redirect(url_for('display_data'))
     return render_template('login.html')
 
-@app.route('/protected')
-def protected():
-    logging.debug('Protected route!')
-    return "This is a protected route!"
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route('/gerenciamento')
+@login_required
+def display_data():
+    return render_template('gerenciamento.html', apenados=apenados)
+
+@app.route('/delete_apenado', methods=['POST'])
+@login_required
+def delete_apenado():
+    if request.method == 'POST':
+        name = request.form['name']
+        if name in apenados:
+            del apenados[name]
+    return redirect(url_for('display_data'))
+
+@app.route('/update_data', methods=['POST'])
+@login_required
+def update_data():
+    if request.method == 'POST':
+        name = request.form['name']
+        new_data = request.form['data']
+        # Update the data in the apenados dictionary
+        apenados[name] = new_data
+        return redirect(url_for('display_data'))
+    
+@app.route('/add_apenado', methods=['POST'])
+@login_required
+def add_apenado():
+    if request.method == 'POST':
+        name = request.form['name']
+        data = request.form['data']
+        # Update the data in the apenados dictionary
+        apenados[name] = data
+        return redirect(url_for('display_data'))
 
 if __name__ == '__main__':
     app.run(debug=True)
