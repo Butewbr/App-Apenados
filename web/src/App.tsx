bb77a98ca4dd61f350d811631879dfece1e3a17a
Fernando Futila <fernando.futila@gmail.com>
2024-04-23 20:12:17 -0300

    feat: Componente Sidebar

---
+++ web/src/App.tsx
@@ -0,0 +1,16 @@
+
+
+
+// import "./global.css"
+
+import PersistentDrawerLeft from "./components/SideMenu"
+
+export function App() {
+  return (
+  <>
+    <PersistentDrawerLeft/>
+  </>
+  )
+}
+
+
