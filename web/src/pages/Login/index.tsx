282deb547f55c8a348550c8addb2cb63c50ab630
Fernando Futila <fernando.futila@gmail.com>
2024-05-09 22:17:17 -0300

    feat: Criação da página de Login

--- web/src/pages/Login/index.tsx
+++ web/src/pages/Login/index.tsx
@@ -1,7 +1,45 @@
+import { Grid,Paper, Avatar, TextField, Button, Typography,Link } from '@mui/material'
+// import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+// import FormControlLabel from '@material-ui/core/FormControlLabel';
+import { FormControlLabel } from '@mui/material';
+import { LockOutlined } from '@mui/icons-material';
+// import Checkbox from '@material-ui/core/Checkbox';
+import { CheckBox } from '@mui/icons-material';
+export function LoginPage (){
 
-
-export function LoginPage() {
-  return (
-    <h1>Login Page</h1>
-  )
+    const paperStyle={padding :20,height:'70vh',width:280, margin:"20px auto"}
+    const avatarStyle={backgroundColor:'#1bbd7e'}
+    const btnstyle={margin:'8px 0'}
+    return(
+        <Grid>
+            <Paper elevation={10} style={paperStyle}>
+                {/* <Grid align='center'>
+                     <Avatar style={avatarStyle}><LockOutlined/></Avatar>
+                    <h2>Sign In</h2>
+                </Grid> */}
+                <TextField label='Username' placeholder='Enter username' variant="outlined" fullWidth required/>
+                <TextField label='Password' placeholder='Enter password' type='password' variant="outlined" fullWidth required/>
+                <FormControlLabel
+                    control={
+                    <CheckBox
+                        name="checkedB"
+                        color="primary"
+                    />
+                    }
+                    label="Remember me"
+                 />
+                <Button type='submit' color='primary' variant="contained" style={btnstyle} fullWidth>Sign in</Button>
+                <Typography >
+                     <Link href="#" >
+                        Forgot password ?
+                </Link>
+                </Typography>
+                <Typography > Do you have an account ?
+                     <Link href="#" >
+                        Sign Up 
+                </Link>
+                </Typography>
+            </Paper>
+        </Grid>
+    )
 }
